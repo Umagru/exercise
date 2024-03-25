f4fdb0aecc04e4c8f88fdb35064d9491f0de1939
Júlio César Bomfim Ferreira <juliocesar.bomfim@hotmail.com>
2024-03-25 18:54:56 -0300

    Update script.js

--- onebitcode/exe002/script.js
+++ onebitcode/exe002/script.js
@@ -4,7 +4,7 @@ Teste de Velocidade
 Escreva um programa em javascript que permita inserir o nome e a velocidade de dois veículos e exiba na tela uma mensagem dizendo qual dos dois é mais rápido (ou que as velocidades são iguais se este for o caso)
 */
 
-/* const vehicle1 = prompt('Enter the first vehicle: ');
+const vehicle1 = prompt('Enter the first vehicle: ');
 const speedVehicle1 = Number(prompt(`Enter "${vehicle1}" speed: `));
 const vehicle2 = prompt('Enter the second vehicle: ');
 const speedVehicle2 = Number(prompt(`Enter "${vehicle2}" speed: `));
@@ -18,7 +18,7 @@ if (speedVehicle1 > speedVehicle2) {
 } else {
     document.getElementById('comparison').innerHTML = 
     `Comparison: The speeds are the same`;
-} */
+}
 
 /* 
 // Teacher resolution
@@ -46,7 +46,7 @@ Escreva um programa que permita inserir o nome e o poder de ataque de um persona
 Por fim, o programa deve subtrair a quantidade de dano da quantidade de pontos de vida do personagem defensor e exibir na tela a quantidade de dano e as informações atualizadas de ambos os personagens.
 */
 
-/* const char1 = prompt('Enter the name of the first character: ');
+const char1 = prompt('Enter the name of the first character: ');
 const damage1 = Number(prompt(`Enter "${char1}" damage: `));
 
 const char2 = prompt('Enter the name of the second character: ');
@@ -72,9 +72,9 @@ document.getElementById('damage').innerHTML =
 `<em>${char1}</em>: <br>Damage = ${damage}`
 
 document.getElementsByClassName('defender')[0].innerHTML =
-`<em>${char2}</em>: <br>Life = ${life}`; */
+`<em>${char2}</em>: <br>Life = ${life}`;
 
-
+/* 
 // Teacher resolution
 
 const char1 = prompt('Enter the name of the first character: ');
@@ -98,4 +98,5 @@ alert(
     char1 + "\nPoder de ataque: " + damage1 + "\n\n" +
     char2 + "\nPontos de vida: " + life +
     "\nPoder de defesa: " + defense + "\nPossui escudo: " + shield
-)
+) 
+*/
